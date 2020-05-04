a0b2adf0848ebd643c1cd43c606246a0ab993b18
vsdudakov <vsdudakov@gmail.com>
2020-05-04 10:30:48 +0300

    Added pages

--- src/containers/Auth/index.js
+++ src/containers/Auth/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import CLIENT_URLS from 'routes/client';
@@ -10,30 +11,35 @@ import ResetPassword from './ResetPassword';
 import ResetPasswordConfirm from './ResetPasswordConfirm';
 
 const Auth = () => (
-  <Switch>
-    <Route
-      path={CLIENT_URLS.AUTH.INDEX.route}
-      exact={true}
-      render={() => <Redirect to={CLIENT_URLS.AUTH.SIGN_IN.buildPath()} />}
-    />
-    <Route exact path={CLIENT_URLS.AUTH.SIGN_IN.route} component={SignIn} />
-    <Route
-      exact={true}
-      path={CLIENT_URLS.AUTH.SIGN_UP.route}
-      component={SignUp}
-    />
-    <Route
-      exact
-      path={CLIENT_URLS.AUTH.RESET_PASSWORD.route}
-      component={ResetPassword}
-    />
-    <Route
-      exact
-      path={CLIENT_URLS.AUTH.RESET_PASSWORD_CONFIRM.route}
-      component={ResetPasswordConfirm}
-    />
-    <NotFoundRoute />
-  </Switch>
+  <>
+    <Helmet>
+      <body className="auth-body" />
+    </Helmet>
+    <Switch>
+      <Route
+        path={CLIENT_URLS.AUTH.INDEX.route}
+        exact={true}
+        render={() => <Redirect to={CLIENT_URLS.AUTH.SIGN_IN.buildPath()} />}
+      />
+      <Route exact path={CLIENT_URLS.AUTH.SIGN_IN.route} component={SignIn} />
+      <Route
+        exact={true}
+        path={CLIENT_URLS.AUTH.SIGN_UP.route}
+        component={SignUp}
+      />
+      <Route
+        exact
+        path={CLIENT_URLS.AUTH.RESET_PASSWORD.route}
+        component={ResetPassword}
+      />
+      <Route
+        exact
+        path={CLIENT_URLS.AUTH.RESET_PASSWORD_CONFIRM.route}
+        component={ResetPasswordConfirm}
+      />
+      <NotFoundRoute />
+    </Switch>
+  </>
 );
 
 export default Auth;
